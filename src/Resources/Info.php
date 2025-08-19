c68062c2370fbb31d25c666d1d7a1136e57c671b
Remco van Essen <remco@productflow.com>
2025-08-19 10:28:41 +0200

    Update new info class

--- src/Resources/Info.php
+++ src/Resources/Info.php
@@ -10,8 +10,18 @@ class Info extends Model
      * @return array
      * @throws KauflandException
      */
-    public function list(): array
+    public function vatIndicators()
     {
         return $this->connection->request('GET', 'info/vat-indicators', ['query' => $this->getQuery()]);
+    }
+
+    public function locale()
+    {
+        return $this->connection->request('GET', 'info/locale', ['query' => $this->getQuery()]);
+    }
+
+    public function storeFront()
+    {
+        return $this->connection->request('GET', 'info/store-front', ['query' => $this->getQuery()]);
     }
 }
